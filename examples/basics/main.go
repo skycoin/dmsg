f6fdfa31179f219c76cfd10b855da06b4bd8444b
Evan Lin <evanlinjin@pm.me>
2019-08-22 08:13:29 +0800

    Cleaned up variable names and comments for example.
    Former-commit-id: dd075575ff48daa1e76b1ba933b93902c5248b20

--- examples/basics/main.go
+++ examples/basics/main.go
@@ -11,111 +11,111 @@ import (
 
 func main() {
 	// generate keys for clients
-	rPK, rSK := cipher.GenerateKeyPair()
-	iPK, iSK := cipher.GenerateKeyPair()
+	respPK, respSK := cipher.GenerateKeyPair()
+	initPK, initSK := cipher.GenerateKeyPair()
 
 	// ports to listen by clients. can be any free port
-	var iPort, rPort uint16 = 1563, 1563
+	var initPort, respPort uint16 = 1563, 1563
 
 	// instantiate discovery
 	dc := disc.NewHTTP("https://messaging.discovery.skywire.skycoin.net")
 
 	// instantiate clients
-	responder := dmsg.NewClient(rPK, rSK, dc)
-	initiator := dmsg.NewClient(iPK, iSK, dc)
+	respC := dmsg.NewClient(respPK, respSK, dc)
+	initC := dmsg.NewClient(initPK, initSK, dc)
 
 	// connect to the DMSG server
-	if err := responder.InitiateServerConnections(context.Background(), 1); err != nil {
+	if err := respC.InitiateServerConnections(context.Background(), 1); err != nil {
 		log.Fatalf("Error initiating server connections by responder: %v", err)
 	}
 
 	// connect to the DMSG server
-	if err := initiator.InitiateServerConnections(context.Background(), 1); err != nil {
+	if err := initC.InitiateServerConnections(context.Background(), 1); err != nil {
 		log.Fatalf("Error initiating server connections by initiator: %v", err)
 	}
 
 	// bind to port and start listening for incoming messages
-	iListener, err := initiator.Listen(iPort)
+	initL, err := initC.Listen(initPort)
 	if err != nil {
-		log.Fatalf("Error listening by initiator on port %d: %v", iPort, err)
+		log.Fatalf("Error listening by initiator on port %d: %v", initPort, err)
 	}
 
 	// bind to port and start listening for incoming messages
-	rListener, err := responder.Listen(rPort)
+	respL, err := respC.Listen(respPort)
 	if err != nil {
-		log.Fatalf("Error listening by responder on port %d: %v", rPort, err)
+		log.Fatalf("Error listening by responder on port %d: %v", respPort, err)
 	}
 
 	// dial responder via DMSG
-	iTp, err := initiator.Dial(context.Background(), rPK, rPort)
+	initTp, err := initC.Dial(context.Background(), respPK, respPort)
 	if err != nil {
 		log.Fatalf("Error dialing responder: %v", err)
 	}
 
-	// accept connection. `AcceptTransport` returns an object exposing `transport` features
+	// Accept connection. `AcceptTransport` returns an object exposing `transport` features
 	// thus, `Accept` could also be used here returning `net.Conn` interface. depends on your needs
-	rTp, err := rListener.AcceptTransport()
+	respTp, err := respL.AcceptTransport()
 	if err != nil {
 		log.Fatalf("Error accepting inititator: %v", err)
 	}
 
-	// initiator writes to his transport
-	_, err = iTp.Write([]byte("Hello there!"))
+	// initiator writes to it's transport
+	_, err = initTp.Write([]byte("Hello there!"))
 	if err != nil {
 		log.Fatalf("Error writing to initiator's transport: %v", err)
 	}
 
-	// responder reads from his transport
-	recBuf := make([]byte, 12)
-	_, err = rTp.Read(recBuf)
+	// responder reads from it's transport
+	recvBuf := make([]byte, 12)
+	_, err = respTp.Read(recvBuf)
 	if err != nil {
 		log.Fatalf("Error reading from responder's transport: %v", err)
 	}
 
-	log.Printf("Responder accepted: %s", string(recBuf))
+	log.Printf("Responder accepted: %s", string(recvBuf))
 
-	// responder writes to his transport
-	_, err = rTp.Write([]byte("General Kenobi"))
+	// responder writes to it's transport
+	_, err = respTp.Write([]byte("General Kenobi"))
 	if err != nil {
 		log.Fatalf("Error writing response: %v", err)
 	}
 
-	// initiator reads from his transport
-	iRecBuf := make([]byte, 14)
-	_, err = iTp.Read(iRecBuf)
+	// initiator reads from it's transport
+	initRecvBuf := make([]byte, 14)
+	_, err = initTp.Read(initRecvBuf)
 	if err != nil {
 		log.Fatalf("Error reading response: %v", err)
 	}
 
-	log.Printf("Initiator accepted: %s", string(iRecBuf))
+	log.Printf("Initiator accepted: %s", string(initRecvBuf))
 
 	// close transport
-	if err := iTp.Close(); err != nil {
+	if err := initTp.Close(); err != nil {
 		log.Fatalf("Error closing initiator's transport: %v", err)
 	}
 
 	// close transport
-	if err := rTp.Close(); err != nil {
+	if err := respTp.Close(); err != nil {
 		log.Fatalf("Error closing responder's transport: %v", err)
 	}
 
 	// close listener
-	if err := iListener.Close(); err != nil {
+	if err := initL.Close(); err != nil {
 		log.Fatalf("Error closing initiator's listener: %v", err)
 	}
 
 	// close listener
-	if err := rListener.Close(); err != nil {
+	if err := respL.Close(); err != nil {
 		log.Fatalf("Error closing responder's listener: %v", err)
 	}
 
 	// close client
-	if err := initiator.Close(); err != nil {
+	if err := initC.Close(); err != nil {
 		log.Fatalf("Error closing initiator: %v", err)
 	}
 
 	// close client
-	if err := responder.Close(); err != nil {
+	if err := respC.Close(); err != nil {
 		log.Fatalf("Error closing responder: %v", err)
 	}
 }
