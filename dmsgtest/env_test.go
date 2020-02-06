035e987517f47168903be4b9308dd5fd5a3dc81b
志宇 <evanlinjin@pm.me>
2020-02-06 11:25:34 +0800

    Added restart_client test for dmsgtest.Env
    Former-commit-id: f1beed02cffd88c0391f2f83f914fceb9965aff3

--- dmsgtest/env_test.go
+++ dmsgtest/env_test.go
@@ -1,7 +1,7 @@
 package dmsgtest
 
 import (
-	"fmt"
+	"context"
 	"testing"
 	"time"
 
@@ -10,26 +10,49 @@ import (
 	"github.com/SkycoinProject/dmsg"
 )
 
-func TestNewEnv(t *testing.T) {
-	cases := []struct {
-		ServerN     int
-		ClientN     int
-		MinSessions int
-	}{
-		{5, 1, 1},
-		{5, 3, 1},
-		{5, 3, 3},
-		{5, 10, 5},
-		{5, 10, 10},
-	}
-	for i, c := range cases {
-		t.Run(fmt.Sprint("case_", i), func(t *testing.T) {
-			env := NewEnv(t, time.Second*30)
+func TestEnv(t *testing.T) {
+
+	const timeout = time.Second * 30
+
+	t.Run("startup_shutdown", func(t *testing.T) {
+		cases := []struct {
+			ServerN     int
+			ClientN     int
+			MinSessions int
+		}{
+			{5, 1, 1},
+			{5, 3, 1},
+			{5, 3, 3},
+			{5, 10, 5},
+			{5, 10, 10},
+		}
+		for i, c := range cases {
+			env := NewEnv(t, timeout)
 			err := env.Startup(c.ServerN, c.ClientN, &dmsg.Config{
 				MinSessions: c.MinSessions,
 			})
-			require.NoError(t, err)
+			require.NoError(t, err, i)
 			env.Shutdown()
-		})
-	}
+		}
+	})
+
+	t.Run("restart_client", func(t *testing.T) {
+		env := NewEnv(t, timeout)
+		require.NoError(t, env.Startup(3, 1, nil))
+		defer env.Shutdown()
+
+		// After closing all clients, n(clients) should be 0.
+		require.Len(t, env.AllClients(), 1)
+		env.CloseAllClients()
+		require.Len(t, env.AllClients(), 0)
+
+		// NewClient should result in n(clients) == 1.
+		// Closing the created client should result in n(clients) == 0 after some time.
+		client, err := env.NewClient(context.TODO(), nil)
+		require.NoError(t, err)
+		require.Len(t, env.AllClients(), 1)
+		require.NoError(t, client.Close())
+		time.Sleep(time.Second)
+		require.Len(t, env.AllClients(), 0)
+	})
 }
