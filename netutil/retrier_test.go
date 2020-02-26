55c83bcd79fa8698342a23cae701ad9394e4a9ac
志宇 <evanlinjin@pm.me>
2020-02-26 22:57:36 +0800

    Added maxBackoff to Retrier.
    * Implemented max-backoff logic.
    
    * Simplified Retrier.
    
    
    Former-commit-id: 215e306ef767c0dd09bfa0606991bdbaedda7e9b

--- netutil/retrier_test.go
+++ netutil/retrier_test.go
@@ -11,7 +11,7 @@ import (
 )
 
 func TestRetrier_Do(t *testing.T) {
-	r := NewRetrier(logrus.New(), time.Millisecond*100, 3, 2)
+	r := NewRetrier(logrus.New(), time.Millisecond*100, 0, 3, 2)
 	c := 0
 	threshold := 2
 	f := func() error {
@@ -43,7 +43,7 @@ func TestRetrier_Do(t *testing.T) {
 
 	t.Run("should return whitelisted errors if any instead of retry", func(t *testing.T) {
 		bar := errors.New("bar")
-		wR := NewRetrier(logrus.New(), 50*time.Millisecond, 1, 2).WithErrWhitelist(bar)
+		wR := NewRetrier(logrus.New(), 50*time.Millisecond, 0, 1, 2).WithErrWhitelist(bar)
 		barF := func() error {
 			return bar
 		}
@@ -54,7 +54,7 @@ func TestRetrier_Do(t *testing.T) {
 
 	t.Run("if times is 0, should retry until success", func(t *testing.T) {
 		c = 0
-		loopR := NewRetrier(logrus.New(), 50*time.Millisecond, 0, 1)
+		loopR := NewRetrier(logrus.New(), 50*time.Millisecond, 0, 0, 1)
 		err := loopR.Do(context.TODO(), f)
 		require.NoError(t, err)
 
