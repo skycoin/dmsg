114e2daf6643ffcfb66c3fbff79931515e0b4c94
Never M <nevermn@protonmail.com>
2021-02-10 16:36:49 +0200

    Add comments to logging middleware

--- httputil/log.go
+++ httputil/log.go
@@ -13,6 +13,9 @@ type structuredLogger struct {
 	logger logrus.FieldLogger
 }
 
+// NewLogMiddleware creates a new instance of logging middleware. This will allow setting
+// adding log fields in the handler and any further middleware. At the end of request, this
+// log entry will be printed at Info level via passed logger
 func NewLogMiddleware(logger logrus.FieldLogger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		fn := func(w http.ResponseWriter, r *http.Request) {
@@ -43,6 +46,9 @@ func NewLogMiddleware(logger logrus.FieldLogger) func(http.Handler) http.Handler
 	}
 }
 
+// LogEntrySetField adds new key-value pair to current (request scoped) log entry. This pair will be
+// printed along with all other pairs when the request is served.
+// This requires log middleware from this package to be installed in the chain
 func LogEntrySetField(r *http.Request, key string, value interface{}) {
 	if sl, ok := r.Context().Value(middleware.LogEntryCtxKey).(*structuredLogger); ok {
 		sl.logger = sl.logger.WithField(key, value)
