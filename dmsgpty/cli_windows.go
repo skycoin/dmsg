f6d6fb4bf6c7c03a1067180b45756402e1db331e
Alexander Adhyatma <alexadhyatma@mailbox.org>
2021-06-21 09:12:49 +0700

    polls windows syscalls for terminal size

--- dmsgpty/cli_windows.go
+++ dmsgpty/cli_windows.go
@@ -4,14 +4,40 @@ package dmsgpty
 
 import (
 	"context"
+	"sync"
+	"time"
 
 	"github.com/ActiveState/termtest/conpty"
 )
 
 // ptyResizeLoop informs the remote of changes to the local CLI terminal window size.
-func ptyResizeLoop(_ context.Context, ptyC *PtyClient) error {
-	// TODO: resize windows pty
-	return nil
+func ptyResizeLoop(ctx context.Context, ptyC *PtyClient) error {
+	t := time.NewTicker(2 * time.Second)
+	mu := sync.RWMutex{}
+	initialSize, err := getSize()
+	if err != nil {
+		return err
+	}
+	for {
+		select {
+		case <-ctx.Done():
+			t.Stop()
+			return nil
+		case <-t.C:
+			mu.Lock()
+			size, err := getSize()
+			if err == nil {
+				if initialSize != size {
+					initialSize = size
+					if err = ptyC.SetPtySize(initialSize); err != nil {
+						mu.Unlock()
+						return err
+					}
+				}
+			}
+			mu.Unlock()
+		}
+	}
 }
 
 func (cli *CLI) prepareStdin() (restore func(), err error) {
