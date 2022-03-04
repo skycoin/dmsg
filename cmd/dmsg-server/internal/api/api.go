4cef17ef8b2ca12a315f4c500f048ced72e2f2de
ersonp <ersonpereiracr7@gmail.com>
2022-03-04 17:17:56 +0530

    Use package httputil from skywire-utilities
    This commit uses the package httputil from the repo skywire-utilities and the package httputil is deleted from dmsg as it is now moved to skywire-utilities.

--- cmd/dmsg-server/internal/api/api.go
+++ cmd/dmsg-server/internal/api/api.go
@@ -16,10 +16,10 @@ import (
 	"github.com/skycoin/skycoin/src/util/logging"
 
 	"github.com/skycoin/dmsg"
-	"github.com/skycoin/dmsg/httputil"
 	"github.com/skycoin/dmsg/servermetrics"
 	"github.com/skycoin/skywire-utilities/pkg/buildinfo"
 	"github.com/skycoin/skywire-utilities/pkg/cipher"
+	"github.com/skycoin/skywire-utilities/pkg/httputil"
 )
 
 // API main object of the server
