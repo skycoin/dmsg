3f61163461eec8a67a5f139c8c2d56a1042f8779
MohammadReza Palide <mrpalide@gmail.com>
2022-05-19 18:51:03 +0430

    improve underload script

--- integration/README.md
+++ integration/README.md
@@ -41,8 +41,8 @@ You need [tmux](https://github.com/tmux/tmux) to continue this test
     ...
     ```
 4. Build new binaries: `make build`
-5. Copy `underLoadLocal.sh` file to skywire clone directory
-6. Run it by `bash underLoadLocal.sh -n 200`
+5. Copy `underlocal.sh` file to skywire clone directory
+6. Run it by `bash underlocal.sh -n 200`
 
 For close all visors and delete generated configs, use these two commands:
 ```
--- integration/underload.sh
+++ integration/underload.sh
@@ -2,25 +2,32 @@
 
 function helpFunction() {
     echo ""
-    echo "Use: $0 -n (max 212)"
+    echo "Use: $0 -n (max 212) -u addr"
     echo "use -n for number of clients"
+    echo "use -u for dsmg discovery URL (optional)"
     exit 1 # Exit script after printing help
 }
 
 function makeConfigs() {
     n=$1
+    u=$2
 
     if [ -z "$n" ]
     then
         echo "-n cannot be empty";
         helpFunction
     fi
-    
+
+    if [ -z "$u" ]
+    then
+        u="localhost:9090"
+    fi
+
     while [ $n -gt 0 ]
     do 
         # Generate config
         ./skywire-cli config gen -two config$n.json --disableapps skysocks,skysocks-client,skychat,vpn-server,vpn-client
-	    sed -i 's/dmsgd.skywire.dev/localhost:9090/gI' config$n.json
+	    sed -i 's/dmsgd.skywire.dev/$u/gI' config$n.json
         # increment the value
         n=`expr $n - 1`
     done
@@ -36,11 +43,12 @@ function startClients() {
     done
 }
 
-while getopts "n:" opt
+while getopts "n:u:" opt
 do
     case "$opt" in
-        n ) numb="$OPTARG" 
-            makeConfigs $numb
+        n ) numb="$OPTARG";;
+        u ) url="$OPTARG"
+            makeConfigs $numb $url
             startClients $numb;;
         ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
     esac
