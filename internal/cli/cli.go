cab4a1a8511d82bf800e5b011155b2bf0c7e1424
Moses Narrow <moe_narrow@use.startmail.com>
2025-01-16 18:20:29 -0600

    adjust comments

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -36,68 +36,39 @@ func StartDmsg(ctx context.Context, dmsgLogger *logging.Logger, pk cipher.PubKey
 	}
 }
 
+//TODO
 /*
-// StartDmsg starts dmsg returns a dmsg client for the given discovery or discoveries
-func StartMultipleDmsg(dmsgLogger *logging.Logger, ctx []context.Context, pk cipher.PubKey, sk cipher.SecKey, httpClient []*http.Client, dmsgDiscs []string, dmsgSessions []int) (dmsgC []*dmsg.Client, stop []func(), errs []error) {
+func startDmsgDirect(ctx context.Context, v *Visor, log *logging.Logger) error { //nolint:all
+	var keys cipher.PubKeys
+	servers := v.conf.Dmsg.Servers
 
-	lengths := map[string]int{
-		"ctx":        len(ctx),
-		"dmsgDiscs":  len(dmsgDiscs),
-		"dmsgSessions": len(dmsgSessions),
-		"httpClient": len(httpClient),
+	if len(servers) == 0 {
+		return nil
 	}
 
-	var referenceLen int
-	for key, length := range lengths {
-		if referenceLen == 0 {
-			referenceLen = length
-		} else if length != referenceLen {
-			errs = append(errs, fmt.Errorf("all arrays input to StartDmsg must be the same length. Mismatch detected:\n%v", lengths))
-			return nil, nil, errs
-		}
+	keys = append(keys, v.conf.PK)
+	entries := direct.GetAllEntries(keys, servers)
+	dClient := direct.NewClient(entries, v.MasterLogger().PackageLogger("dmsg_http:direct_client"))
+
+	dmsgDC, closeDmsgDC, err := direct.StartDmsg(ctx, v.MasterLogger().PackageLogger("dmsg_http:dmsgDC"),
+		v.conf.PK, v.conf.SK, dClient, dmsg.DefaultConfig())
+	if err != nil {
+		return fmt.Errorf("failed to start dmsg: %w", err)
 	}
 
-	// Validate `dmsgDiscs` for duplicates and empty strings
-	seen := make(map[string]bool)
-	for _, disc := range dmsgDiscs {
-		if disc == "" {
-			errs = append(errs, fmt.Errorf("dmsgDiscs contains an empty string"))
-			continue
-		}
-		if seen[disc] {
-			errs = append(errs, fmt.Errorf("dmsgDiscs contains duplicate entry: %q", disc))
-		}
-		seen[disc] = true
-	}
+	dmsgHTTP := http.Client{Transport: dmsghttp.MakeHTTPTransport(ctx, dmsgDC)}
 
-	// Return errors if validation fails
-	if len(errs) > 0 {
-		return nil, nil, errs
-	}
+	v.pushCloseStack("dmsg_http", func() error {
+		closeDmsgDC()
+		return nil
+	})
 
-
-  for i, _ := range dmsgDisc {
-	dmsgC = append(dmsgc, dmsg.NewClient(pk, sk, disc.NewHTTP(dmsgDisc[i], httpClient, dmsgcurlLog), &dmsg.Config{MinSessions: dmsgSessions[i]}))
-	go dmsgC[i].Serve(ctx[i])
-
-	stop = append(stop, func() {
-		err := dmsgC.Close()
-		dmsgLogger.WithError(err).Debug("Disconnected from dmsg network.")
-		fmt.Printf("\n")
-	}
-	dmsgLogger.WithField("public_key", pk.String()).WithField("dmsg_disc", dmsgDisc[i]).
-		Debug("Connecting to dmsg network...")
-
-	select {
-	case <-ctx[i].Done():
-		stop[i]()
-    errs = append(errs,ctx[i].Err())
-		return nil, nil, errs
-
-	case <-dmsgC[i].Ready():
-		dmsgLogger.Debug("Dmsg network ready for client: %d",i)
-	}
-  return dmsgC, stop, nil
-}
+	v.initLock.Lock()
+	v.dClient = dClient
+	v.dmsgHTTP = &dmsgHTTP
+	v.dmsgDC = dmsgDC
+	v.initLock.Unlock()
+	time.Sleep(time.Duration(len(entries)) * time.Second)
+	return nil
 }
 */
