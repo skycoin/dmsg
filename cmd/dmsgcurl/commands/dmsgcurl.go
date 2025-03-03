64850608d8648b96507197f3def3d5f1b2db5442
Moses Narrow <moe_narrow@use.startmail.com>
2025-03-03 10:52:39 -0600

    remove unneeded logging

--- cmd/dmsgcurl/commands/dmsgcurl.go
+++ cmd/dmsgcurl/commands/dmsgcurl.go
@@ -159,7 +159,6 @@ func handleRequest(ctx context.Context, dmsgLogger *logging.Logger, pk cipher.Pu
 		dmsgC, closeDmsg, err = cli.StartDmsg(ctx, dmsgLogger, pk, sk, httpClient, dmsgDisc, dmsgSessions)
 	} else {
 		dmsgC, closeDmsg, err = cli.StartDmsgDirect(ctx, dmsgLogger, pk, sk, httpClient, dmsgDisc, dmsgSessions)
-		dmsgcurlLog.Debug(parsedURL)
 	}
 	if err != nil {
 		dmsgcurlLog.WithError(err).Fatal("Failed to start dmsg\n")
