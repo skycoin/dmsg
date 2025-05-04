b3f1d5dedd906a9f3b92f881a2efc2896cf11766
Mohammed <mrpalide@gmail.com>
2025-05-04 14:07:03 +0000

    remove //TODO comment

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -38,8 +38,6 @@ func StartDmsg(ctx context.Context, dmsgLogger *logging.Logger, pk cipher.PubKey
 	}
 }
 
-//TODO
-
 func StartDmsgDirect(ctx context.Context, dmsgLogger *logging.Logger, pk cipher.PubKey, sk cipher.SecKey, httpClient *http.Client, _ string, dmsgSessions int, destination string) (dmsgC *dmsg.Client, stop func(), err error) { //nolint:all
 	var servers []*disc.Entry
 	for i := range dmsg.Prod.DmsgServers {
